70850d4c5c270323a70ecaad4e62793ba4999b2f
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-12-08 03:00:35 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,21 +3,28 @@
 LOGFILE=/opt/iobroker/log/iobroker.%Y-%m-%d.log
 SLEEPINTERVAL=1.0
 
-test -f "$(date +"$LOGFILE")" && OLDLINES=$(wc -l < "$(date +"$LOGFILE")") || OLDLINES=0
-NEWLINES=$OLDLINES
-FOUND=false
-function PrintNewLines {
-        	NEWLINES=$(wc -l < "$(date +"$LOGFILE")")
-        	(( OLDLINES <= NEWLINES )) && LINES=$(( NEWLINES - OLDLINES )) || LINES=$NEWLINES
-        	tail -n "$LINES" "$(date +"$LOGFILE")"
-        	test ! -z "$1" && grep -q "$1" <(tail -n "$LINES" "$(date +"$LOGFILE")") && FOUND=true || FOUND=false
-        	OLDLINES=$NEWLINES
-}
-
-while true; do
+if [ -f $LOGFILE ]
+then
+	test -f "$(date +"$LOGFILE")" && OLDLINES=$(wc -l < "$(date +"$LOGFILE")") || OLDLINES=0
+	NEWLINES=$OLDLINES
+	FOUND=false
+	function PrintNewLines {
+	        NEWLINES=$(wc -l < "$(date +"$LOGFILE")")
+	        (( OLDLINES <= NEWLINES )) && LINES=$(( NEWLINES - OLDLINES )) || LINES=$NEWLINES
+	        tail -n "$LINES" "$(date +"$LOGFILE")"
+	        test ! -z "$1" && grep -q "$1" <(tail -n "$LINES" "$(date +"$LOGFILE")") && FOUND=true || FOUND=false
+	        OLDLINES=$NEWLINES
+	}
+	
+	while true; do
 		PrintNewLines
 		sleep $SLEEPINTERVAL
-done
-
+	done
+else
+	echo "NO LOGFILE!"
+	while true; do
+		
+	done
+fi
 
 #node node_modules/iobroker.js-controller/controller.js
