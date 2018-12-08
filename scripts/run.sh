399f07dcf8dabbe6e4c5c13123edad3cbc81d9bb
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-12-08 05:11:16 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,28 +1,14 @@
 #!/bin/sh
 
-#LOGFILE=/opt/iobroker/log/iobroker.%Y-%m-%d.log
-#SLEEPINTERVAL=1.0
+if [ "$(ls -A /opt/iobroker/)" ]; then
+        
+	echo "Directory /opt/iobroker/ isn't empty, no extraction processed!"
+else
+        
+	echo "Directory /opt/iobroker/ is empty, extracting config now..."
+	tar -xkf /root/iobrokerBase.tgz -C /opt/iobroker/
+	echo
+	echo "Extracted package $PACKAGE to /opt/iobroker/ to initialize the configuration directory."
+fi
 
-#if [ -f $LOGFILE ]
-#then
-#	test -f "$(date +"$LOGFILE")" && OLDLINES=$(wc -l < "$(date +"$LOGFILE")") || OLDLINES=0
-#	NEWLINES=$OLDLINES
-#	FOUND=false
-#	function PrintNewLines {
-#	        NEWLINES=$(wc -l < "$(date +"$LOGFILE")")
-#	        (( OLDLINES <= NEWLINES )) && LINES=$(( NEWLINES - OLDLINES )) || LINES=$NEWLINES
-#	        tail -n "$LINES" "$(date +"$LOGFILE")"
-#	        test ! -z "$1" && grep -q "$1" <(tail -n "$LINES" "$(date +"$LOGFILE")") && FOUND=true || FOUND=false
-#	        OLDLINES=$NEWLINES
-#	}
-#	
-#	while true; do
-#		PrintNewLines
-#		sleep $SLEEPINTERVAL
-#	done
-#else
-	echo "NO LOGFILE!"
-	tail -f /dev/null
-#fi
-
-#node node_modules/iobroker.js-controller/controller.js
+node node_modules/iobroker.js-controller/controller.js
