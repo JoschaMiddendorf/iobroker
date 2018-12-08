680c09fdbf0cd737852629aa6dc23a7cb63a82e8
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-12-08 03:05:44 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
-LOGFILE=/opt/iobroker/log/iobroker.%Y-%m-%d.log
-SLEEPINTERVAL=1.0
+#LOGFILE=/opt/iobroker/log/iobroker.%Y-%m-%d.log
+#SLEEPINTERVAL=1.0
 
-if [ -f $LOGFILE ]
-then
-	test -f "$(date +"$LOGFILE")" && OLDLINES=$(wc -l < "$(date +"$LOGFILE")") || OLDLINES=0
-	NEWLINES=$OLDLINES
-	FOUND=false
-	function PrintNewLines {
-	        NEWLINES=$(wc -l < "$(date +"$LOGFILE")")
-	        (( OLDLINES <= NEWLINES )) && LINES=$(( NEWLINES - OLDLINES )) || LINES=$NEWLINES
-	        tail -n "$LINES" "$(date +"$LOGFILE")"
-	        test ! -z "$1" && grep -q "$1" <(tail -n "$LINES" "$(date +"$LOGFILE")") && FOUND=true || FOUND=false
-	        OLDLINES=$NEWLINES
-	}
-	
-	while true; do
-		PrintNewLines
-		sleep $SLEEPINTERVAL
-	done
-else
+#if [ -f $LOGFILE ]
+#then
+#	test -f "$(date +"$LOGFILE")" && OLDLINES=$(wc -l < "$(date +"$LOGFILE")") || OLDLINES=0
+#	NEWLINES=$OLDLINES
+#	FOUND=false
+#	function PrintNewLines {
+#	        NEWLINES=$(wc -l < "$(date +"$LOGFILE")")
+#	        (( OLDLINES <= NEWLINES )) && LINES=$(( NEWLINES - OLDLINES )) || LINES=$NEWLINES
+#	        tail -n "$LINES" "$(date +"$LOGFILE")"
+#	        test ! -z "$1" && grep -q "$1" <(tail -n "$LINES" "$(date +"$LOGFILE")") && FOUND=true || FOUND=false
+#	        OLDLINES=$NEWLINES
+#	}
+#	
+#	while true; do
+#		PrintNewLines
+#		sleep $SLEEPINTERVAL
+#	done
+#else
 	echo "NO LOGFILE!"
 	while true; do
 		
 	done
-fi
+#fi
 
 #node node_modules/iobroker.js-controller/controller.js
