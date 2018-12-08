4f827fd5cda17c8f3bd5c67057a3db9a29d55003
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-12-08 06:15:07 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,15 +12,15 @@
 ##
 ##################################################################################################
 
-if [ "$(ls -A /opt/iobroker/)" ]; then
+#if [ "$(ls -A /opt/iobroker/)" ]; then
         
-	echo "Directory /opt/iobroker/ isn't empty, no extraction processed!"
-else
+#	echo "Directory /opt/iobroker/ isn't empty, no extraction processed!"
+#else
         
-	echo "Directory /opt/iobroker/ is empty, extracting config now..."
-	tar -xkf /root/iobrokerBase.tgz -C /opt/iobroker/
-	echo "Extracted package $PACKAGE to /opt/iobroker/ to initialize the configuration directory."
-fi
+#	echo "Directory /opt/iobroker/ is empty, extracting config now..."
+#	tar -xkf /root/iobrokerBase.tgz -C /opt/iobroker/
+#	echo "Extracted package $PACKAGE to /opt/iobroker/ to initialize the configuration directory."
+#fi
 
 tail -f /dev/null
 #node /opt/iobroker/node_modules/iobroker.js-controller/controller.js
