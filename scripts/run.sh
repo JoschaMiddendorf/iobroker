a30cb79c27e8397c2a81641670077766fcd6bcb0
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-12-08 07:19:52 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -12,15 +12,18 @@
 ##
 ##################################################################################################
 
-#if [ "$(ls -A /opt/iobroker/)" ]; then
+if [ "$(ls -A /opt/iobroker/)" ]; then
         
-#	echo "Directory /opt/iobroker/ isn't empty, no extraction processed!"
-#else
-        
-#	echo "Directory /opt/iobroker/ is empty, extracting config now..."
-#	tar -xkf /root/iobrokerBase.tgz -C /opt/iobroker/
-#	echo "Extracted package $PACKAGE to /opt/iobroker/ to initialize the configuration directory."
-#fi
+        echo "Directory /opt/iobroker/ isn't empty, no instalation neded, startig IoBroker now."
+	#echo "Directory /opt/iobroker/ isn't empty, no extraction processed!"
+else
+        echo "Directory /opt/iobroker/ is empty, installing IoBroker now..."
+        npm install iobroker --unsafe-perm && npm i --production --unsafe-perm
+        echo "Installed IoBroker, startig IoBroker now."
+        #echo "Directory /opt/iobroker/ is empty, extracting config now..."
+        #tar -xkf /root/iobrokerBase.tgz -C /opt/iobroker/
+        #echo "Extracted package $PACKAGE to /opt/iobroker/ to initialize the configuration directory."
+fi
 
-tail -f /dev/null
-#node /opt/iobroker/node_modules/iobroker.js-controller/controller.js
+#tail -f /dev/null
+node /opt/iobroker/node_modules/iobroker.js-controller/controller.js
