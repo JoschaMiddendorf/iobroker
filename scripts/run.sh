28d5d34738b844fd4bdf704195572a3d9e86f521
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-12-08 05:16:01 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,7 +7,6 @@ else
         
 	echo "Directory /opt/iobroker/ is empty, extracting config now..."
 	tar -xkf /root/iobrokerBase.tgz -C /opt/iobroker/
-	echo
 	echo "Extracted package $PACKAGE to /opt/iobroker/ to initialize the configuration directory."
 fi
 
