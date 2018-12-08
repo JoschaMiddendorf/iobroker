71f3514661dc7344b5943bd0ab0c9c0fea56dc92
Joscha Middendorf <Joscha.middendorf@gmail.com>
2018-12-08 02:33:05 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-node node_modules/iobroker.js-controller/controller.js
+LOGFILE=/opt/iobroker/log/iobroker.%Y-%m-%d.log
+SLEEPINTERVAL=1.0
+
+function PrintNewLines {
+        	NEWLINES=$(wc -l < "$(date +"$LOGFILE")")
+        	(( OLDLINES <= NEWLINES )) && LINES=$(( NEWLINES - OLDLINES )) || LINES=$NEWLINES
+        	tail -n "$LINES" "$(date +"$LOGFILE")"
+        	test ! -z "$1" && grep -q "$1" <(tail -n "$LINES" "$(date +"$LOGFILE")") && FOUND=true || FOUND=false
+        	OLDLINES=$NEWLINES
+}
+
+while true; do
+		PrintNewLines											## Printing log lines in intervalls
+		sleep $SLEEPINTERVAL
+done
+
+
+#node node_modules/iobroker.js-controller/controller.js
